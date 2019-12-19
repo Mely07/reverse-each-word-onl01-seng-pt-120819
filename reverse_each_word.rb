99cb45c056c69f74b4b5f59c8893c8765b991d0f
Damely Tineo <damely.tineo07@gmail.com>
2019-12-19 01:31:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,8 @@ def reverse_each_word(sentence)
  sentence_to_array = sentence.split(" ")
  reversed_sentence = [ ]
  sentence_to_array.each do |word|
-
- reversed_sentence << word.reverse
+  reversed_sentence << word.reverse
  end
  return reversed_sentence.join(" ")
-
 end
+
